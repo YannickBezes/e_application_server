d74b9286d9f44ac3e527a346de7c149689132015
YannickBezes <yannick.bezes@gmail.com>
2019-09-27 01:55:57 +0200

    encode request in utf8, get nodes types, entries, relations types and outcoming relations

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,41 @@
-import http from 'http'
+import request from 'request'
+import iconv from 'iconv-lite'
 
 export default app => {
   app.get('/:mot', (req, res) => get_mot(req, res))
 }
 
 function get_mot(req, res) {
-  let data = null
-  res.set('charset', 'UTF-8')
-  http.get(
-    `http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=${req.params.mot}`,
-    response => {
-      response.on('data', d => {
-        data += d
-      })
-      response.on('end', () => {
-        console.log(data)
-        res.json({ status: 'success', data: parse_response(data) })
-      })
-    }
-  )
+  request.get({encoding: null, uri:`http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=${req.params.mot}&rel=1`}, (error, response, body) => {
+    var utf8String = iconv.decode(new Buffer(body), "ISO-8859-1");
+    res.json({status: 'success', data: parse_response(utf8String)})
+  })
 }
 
 function parse_response(d) {
-  return d.substring(d.indexOf('<CODE>') + 6, d.indexOf('</CODE>'))
+  let str = d.substring(d.indexOf('<CODE>') + 6, d.indexOf('</CODE>'))
+  
+  // Get the nodes types
+  let nodes_types = parse(str, 'nt')
+  // Get the entries
+  let entries = parse(str, 'e')
+  // Get the relations types
+  let relations_types = parse(str, 'rt')
+  // Get the outcoming relations
+  let outcoming_relations = parse(str, 'r')
+
+  return {nodes_types, entries, relations_types, outcoming_relations}
+}
+
+
+function parse(str, regex) {
+  let trash = str.match(new RegExp(`${regex};.+\n`, 'g'))
+  let clean = []
+
+  trash.forEach(el => {
+    // Clean entries (remove the entries which aren't it, remove the '\n')
+    console.log(el)
+    if (/\d/.test(el.split(';')[1])) clean.push(el.substring(0, el.length - 1))
+  })
+  return clean
 }
