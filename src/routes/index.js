0964225563dc7566ce2c9c8cbaa8a419c54ef195
yannickbezes <yannick.bezes@gmail.com>
2019-10-03 18:20:53 +0200

    add cache, parse def

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,34 +2,66 @@ import request from 'request'
 import iconv from 'iconv-lite'
 import fs from 'fs'
 
+const CACHE = true // Activate cache
+
 export default app => {
-  app.get('/:word', (req, res) => get_word(req, res))
+  app.get('/:word/definition', (req, res) => get_def(req, res)) 
   app.get('/:word/:rel', (req, res) => get_word(req, res))
 }
 
 function get_word(req, res) {
   try {
-    if (fs.existsSync(`cache/${req.params.word}.json`)) {
-      fs.readFile(`cache/${req.params.word}.json`, 'utf8', (err, data) => {
+    if (fs.existsSync(`cache/${req.params.word}_${req.params.rel}.json`) && CACHE) {
+      fs.readFile(`cache/${req.params.word}_${req.params.rel}.json`, 'utf8', (err, data) => {
         res.send({ status: 'success', data: JSON.parse(data) })
       })
     } else {
       request.get({ encoding: null, uri: `http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=${req.params.word}&rel=${req.params.rel}` }, (error, response, body) => {
         if (response.statusCode == 200) {
           // Convert ISO to utf8
-          let utf8_string = iconv.decode(new Buffer(body), "ISO-8859-1");
+          let utf8_string = iconv.decode(new Buffer(body), "ISO-8859-1")
           let data = parse_response(utf8_string)
-          save_cache(req.params.word, data)
-          if (data)
+          
+          if (data) {
             res.json({ status: 'success', data })
-          else
+            save_cache(`${req.params.word}_${req.params.rel}`, data)
+          } else
             res.json({ status: 'failed', data: null, message: 'Not found' })
+          
         } else
           res.json({ status: 'failed', data: null, message: 'Not found' })
       })
     }
   } catch (err) {
-    console.error(err)
+    res.json({status: 'failed', data: null, message: err})
+  }
+}
+
+function get_def(req, res) {
+  try {
+    if (fs.existsSync(`cache/${req.params.word}_def.json`) && CACHE) {
+      fs.readFile(`cache/${req.params.word}_def.json`, 'utf8', (err, data) => {
+        res.send({ status: 'success', data: JSON.parse(data) })
+      })
+    } else {
+      request.get({ encoding: null, uri: `http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=${req.params.word}&rel=4` }, (error, response, body) => {
+        if (response.statusCode == 200) {
+          // Convert ISO to utf8
+          let utf8_string = iconv.decode(new Buffer(body), "ISO-8859-1")
+          let data = parse_definition(utf8_string)
+          
+          if (data) {
+            res.json({ status: 'success', data })
+            save_cache(`${req.params.word}_def`, data)
+          } else
+            res.json({ status: 'failed', data: null, message: 'Not found' })
+          
+        } else
+          res.json({ status: 'failed', data: null, message: 'Not found' })
+      })
+    }
+  } catch (error) {
+    console.error(error)
   }
 }
 
@@ -37,23 +69,42 @@ function parse_response(d) {
   let str = d.substring(d.indexOf('<CODE>') + 6, d.indexOf('</CODE>'))
   if (str.trim() != '') {
     // Get the nodes types
-    let nodes_types = parse(str, 'nodes_types')
+    // let nodes_types = parse(str, 'nodes_types')
+
     // Get the entries
     let entries = parse(str, 'entries')
+    let entries_dic = {}
+    entries.forEach(el => {
+
+      let name = el.split(';')[2].substring(1, el.split(';')[2].length - 1)
+      if (/>/.test(name)) name = el.split(';')[5].substring(1, el.split(';')[5].length - 1)
+      entries_dic[el.split(';')[1]] = name
+    })
+
     // Get the relations types
-    let relations_types = parse(str, 'relation_types')
+    // let relations_types = parse(str, 'relation_types')
+
     // Get the outcoming relations
     let outcoming_relations = parse(str, 'outcomming_relations')
+    outcoming_relations = parse_relations(outcoming_relations, entries_dic)
+    outcoming_relations.sort((a,b) => { // Sort
+      return parseInt(b.split(';')[2]) - parseInt(a.split(';')[2])
+    })
+
     // Get the incomming relations
     let incomming_relations = parse(str, 'incomming_relations')
-
-    return { nodes_types, entries, relations_types, outcoming_relations, incomming_relations }
+    incomming_relations = parse_relations(incomming_relations, entries_dic)
+    incomming_relations.sort((a,b) => { // Sort
+      return parseInt(b.split(';')[2]) - parseInt(a.split(';')[2])
+    })
+ 
+    return { outcoming_relations, incomming_relations }
   }
   return null
 }
 
 function parse(str, type) {
-  let start, end, regex;
+  let start, end, regex
   switch (type) {
     case "nodes_types":
       start = "// les types de noeuds (Nodes Types) : nt;ntid;'ntname'"
@@ -92,15 +143,36 @@ function parse(str, type) {
   if (trash) {
     trash.forEach(el => {
       // Clean entries (remove the entries which aren't it, remove the '\n')
-      if (/\d/.test(el.split(';')[1])) clean.push(el.substring(0, el.length - 1))
+      if (/\d/.test(el.split(';')[1])) clean.push(el.substring(0, el.length - 1)) // Remove the \n
     })
   }
   return clean
 }
 
-function save_cache(word, data) {
-  fs.writeFile(`cache/${word}.json`, JSON.stringify(data), function (err) {
-    if (err) throw err;
-    console.log('File is created successfully.');
-  });
+function save_cache(name, data) {
+  fs.writeFile(`cache/${name}.json`, JSON.stringify(data), function (err) {
+    if (err) console.error(err)
+    console.log('File is created successfully.') // DEBUG
+  })
+}
+
+function parse_relations(relations, entries) {
+  let relations_obj = []
+  relations.forEach(el => {
+      relations_obj.push(`${el.split(';')[4]};${entries[el.split(';')[3]]};${el.split(';')[5]}`)
+  })
+
+  return relations_obj
+}
+
+function parse_definition(str) {
+  let def = str.substring(str.indexOf('<def>') + 5, str.indexOf('</def>')).replace(/<br \/>/g, '')
+  if(def.trim() != '') {
+    def = def.trim().split('\n')
+    def = {definition: def.map(el => {
+      return el.replace(/\d+\. /, '').trim()
+    })}
+  } else def = []
+
+  return def
 }
